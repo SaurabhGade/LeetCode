3bda975a9b6a8914d832b4f8f88460b64a0ac6b1
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-06-18 11:53:20 +0530

    Time: 73 ms (93.05%) | Memory: 95.9 MB (9.33%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,10 @@ public:
          int min = prices[0];
         int ans = 0;
         for(int i = 1 ; i < prices.size() ; i++){
-            if(min > prices[i])
-                min = prices[i];
-            int curr = prices[i]-min;
+            int temp = prices[i];
+            if(min > temp)
+                min = temp;
+            int curr = temp-min;
             if(ans < curr)
                 ans = curr;
         }
