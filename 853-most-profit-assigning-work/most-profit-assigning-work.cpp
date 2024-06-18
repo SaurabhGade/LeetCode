89f28bcf14ede083256ddafb71f63d89229eb75b
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-06-18 11:37:36 +0530

    Time: 61 ms (39.05%) | Memory: 41.1 MB (57.35%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        vector<pair<int , int> > vec;
+        for(int i = 0 ; i < profit.size() ; i++)
+            vec.push_back({difficulty[i] , profit[i]});
+        sort(vec.begin() , vec.end());
         int max = 0;
-        for(int i = 0 ; i < worker.size() ; i++){
-            int curr = 0;
-            for(int j = 0 ; j < difficulty.size() ; j++){
-                if(worker[i] >= difficulty[j]){
-                    if(curr < profit[j]){
-                        curr = profit[j];
-                    }
-                }
-            }
-            max += curr;
+        for(int i = 0 ; i < vec.size() ; i++){
+            if(max > vec[i].second)
+                vec[i].second = max;
+            else
+                max = vec[i].second;
         }
-        return max;
+        max = 0;
+        int j = 0;
+        int mxp = 0;
+        sort(worker.begin() , worker.end());
+        for(int i = 0 ; i < worker.size() ; i++){
+           while (j < vec.size() && worker[i] >= vec[j].first){
+            max = vec[j].second;
+            j++;
+           }
+            mxp += max;
+        }
+        return mxp;
     }
 };
