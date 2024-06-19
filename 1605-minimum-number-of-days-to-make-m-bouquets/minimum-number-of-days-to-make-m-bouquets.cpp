66c32c5052acbb4031ae1621ff347111c9e4cdeb
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-06-19 19:00:49 +0530

    Time: 126 ms (11.95%) | Memory: 69 MB (28.78%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    bool isPossible(vector<int> & bloomDay , int m , int k , int d){
+        int cnt  = 0;
+        int ms = 0;
+        int n = bloomDay.size();
+        for(int i = 0 ; i < n ; i++){
+            if(bloomDay[i] <= d)
+                cnt++;
+            else{
+                ms += cnt/k;
+                cnt = 0;
+            }
+        }
+        ms += cnt/k;
+        if(ms >= m)
+        return true;
+        return false;
+    }
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
+        if(double(n) < double(m)*double(k))
+            return -1;
+        int ans = INT_MAX;
+        int min = INT_MAX , max = INT_MIN;
+        for(int i = 0 ; i < n ; i++){
+            if(bloomDay[i] < min)
+                min = bloomDay[i];
+            if(bloomDay[i] > max)
+                max = bloomDay[i];
+        }
+
+        while(min <= max){
+            int mid = (min + max)/2;
+            if(isPossible(bloomDay , m , k , mid)){
+                if(ans > mid)
+                    ans = mid;
+                max = mid-1;
+            }else
+                min = mid+1;
+        }
+        return min;
+
+
+       /*   f*cking TLE
+        int n = bloomDay.size();
+         if(m*k > n)
+            return -1;
+        unordered_set<int> st(bloomDay.begin() , bloomDay.end());
+        int ans = 0;
+        priority_queue<int , vector<int> , greater<int> > pq(st.begin() , st.end());
+        while(!pq.empty()){
+            int cnt = 0;
+             ans = pq.top(); pq.pop();
+            int ks = 0;
+            for(int i = 0 ; i < n  ; i++){
+                if(bloomDay[i] - ans <= 0){
+                    cnt++;
+                }else{
+                    ks += cnt/k;
+                    cnt = 0;
+                    if(ks >= m){
+                        return ans;
+                    }
+                }
+            }
+            ks += cnt/k;
+            if(ks >= m)
+                return ans;
+        }
+        return ans;
+        */
+        
+    }
+};
