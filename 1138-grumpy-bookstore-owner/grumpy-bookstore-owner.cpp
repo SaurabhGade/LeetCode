5bea7f3498280224d2c5c05db36e025eb0e16750
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-06-21 15:32:36 +0530

    Time: 21 ms (76.97%) | Memory: 34.3 MB (82.09%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int m) {
-        int ans = 0;
+        int ans = 0 , curr = 0 , inc = 0;
         int n = grumpy.size();
         for(int i = 0 ; i < n ; i++){
-            if(grumpy[i] != 1)
+            if(grumpy[i] != 1){
                 ans += customers[i];
-        }
-        int inc = 0;
-        for(int i = 0 ; i <= n-m ; i++){
-            int curr = 0;
-            for(int j = i ; j < i+m && j < n; j++){
-                if(grumpy[j] != 0)
-                    curr += customers[j];
+                customers[i] = 0;
             }
-            if(curr > inc)
-                inc = curr;
+            if(i >= m){
+                 inc = (curr > inc)?curr:inc;
+                if(grumpy[i-m] != 0){
+                    curr -= customers[i-m];
+                }  
+            }
+            curr += customers[i];
         }
+        inc = (curr > inc)? curr : inc;
         return ans+inc;
+        
     }
 };
