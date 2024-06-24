bd82390af5acef64999fe431d1ed78c32b0e3182
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-06-24 16:40:29 +0530

    Time: 2 ms (80.84%) | Memory: 7 MB (95.08%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0)
-            return 0;
-        if(n == 1)
-            return 1;
-        return fib(n-1)+fib(n-2);
+        int psum = 0;
+        int sum = 1;
+        if(n == 0) return psum;
+        if(n == 1) return sum;
+        int next = 0;
+        for(int i = 2; i <= n  ; i++){
+            next = psum + sum;
+            psum = sum;
+            sum = next;
+        }
+        return next;
     }
 };
