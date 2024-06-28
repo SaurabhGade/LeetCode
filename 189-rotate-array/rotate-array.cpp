596e8c11db146c190f9c04292d899256798c6613
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-06-28 14:40:15 +0530

    Time: 19 ms (70.52%) | Memory: 29.6 MB (5.89%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,9 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         vector<int> aux;
         int n = nums.size();
-        if(k > n){
-            k = k % n;
-        }
+        if(k > n) k = k % n;    //k should not be the more than the size of vector.
         int m = n-k;
         for(m ; m < n ; m++)
             aux.push_back(nums[m]);
