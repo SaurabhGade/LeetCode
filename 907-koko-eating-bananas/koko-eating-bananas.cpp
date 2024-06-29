e834ccc73ccb93ad63ca7a5985885c4b66a0d6a0
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-06-29 15:03:26 +0530

    Time: 25 ms (88.97%) | Memory: 21.1 MB (99.18%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    bool isPossible(vector<int> & piles , int h , unsigned long k){
-        unsigned long time = 0;
-        for(int i = 0 ; i < piles.size() ; i++){
-            unsigned long t = piles[i]/k;
-            time += (piles[i]%k)?t+1:t;
+    bool isPossible(vector<int>& piles, int h,
+                    long long int m) { // m is rate of eating banana
+        long long int time = 0;
+        for (int i = 0; i < piles.size(); i++) {
+            int k = piles[i] / m;
+            time += (piles[i] % m == 0) ? k : k + 1;
         }
         return time <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        /// TLE use binary search.
-        // int k = 1;
-        // while(!isPossible(piles , h , k))
-        //     k++;
-        // return k;
-        unsigned long min  = 0;
+
+        unsigned long min = 0;
         unsigned long max = 0;
-        for(int i = 0 ; i < piles.size() ; i++)
-                max += piles[i];
-        
-        min = max/h; if(min <= 0) min = 1;
-        while(min <= max){
-            unsigned long mid = (min+max)/2;
-            if(isPossible(piles, h , mid)){
-                max = mid-1;
-            }else{
-                min = mid+1;
+        for (int i = 0; i < piles.size(); i++) {
+            if (piles[i] > max) {
+                max = piles[i];
             }
+        }
+        min = max / h;
+        if (min == 0)
+            min = 1;
+        while (min <= max) {
+            long long int mid = (min + max) / 2;
+            if (isPossible(piles, h, mid))
+                max = mid - 1;
+            else
+                min = mid + 1;
         }
         return min;
     }
