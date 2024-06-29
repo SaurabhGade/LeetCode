5269f5dc69bfe5e91bf92cb2f21f2af2ea8b6829
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-06-29 08:04:32 +0530

    Time: 17 ms (73.34%) | Memory: 24.7 MB (13.95%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,10 +1,44 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = nums[0];
-        for(int i = 1 ; i < nums.size(); i++){
-            ans ^= nums[i];
+        int n = nums.size();
+        int i = 0 , j = nums.size()-1;
+        int mid;
+        while(i <= j){
+            mid = (i+j)/2;
+            if(i == j)
+                return nums[mid];
+            if(mid == 0 && nums[mid] != nums[mid+1])
+                return nums[mid];
+            else if(mid == n-1 && nums[mid] != nums[mid-1])
+                return nums[mid];
+            else
+                if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
+                    return nums[mid];
+            
+            // if(i == j)
+            //     return nums[mid];
+            // if(i > 0 && nums[i] != nums[i+1] && nums[i] != nums[i-1])
+            //     return nums[i];
+            // if(i < n-1 && nums[j] != nums[j+1] && nums[j] != nums[j-1])
+            //     return nums[j];
+            if(mid % 2 == 0){
+                if(mid >= n-1)
+                    return nums[mid];
+                else if(nums[mid] == nums[mid+1])
+                    i = mid+1;
+                else
+                    j = mid-1;
+            }
+            else{
+                if(mid <= 0)
+                    return nums[mid];
+                else if(nums[mid] == nums[mid-1])
+                    i = mid+1;
+                else
+                    j = mid-1;
+            }
         }
-        return ans;
+        return nums[mid];
     }
 };
