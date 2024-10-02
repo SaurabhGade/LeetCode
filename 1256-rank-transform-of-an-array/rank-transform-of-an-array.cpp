feebaaa5d205e560933fde06d6d85ea699ffb3b8
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-10-02 09:04:04 +0530

    Time: 43 ms (94.17%) | Memory: 39.1 MB (85.62%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> ans(arr.size());
+        if(!ans.size()) return ans;
+        vector<pair<int, int>> pr;
+        for(int i = 0 ; i < arr.size() ; i++)
+            pr.push_back({arr[i], i});
+        sort(pr.begin(), pr.end());
+        int rank = 1;
+        int prv = pr[0].first;
+        for(int i = 0; i < pr.size(); i++){
+            rank += (prv == pr[i].first)? 0: 1;
+            ans[pr[i].second] = rank;
+            prv = pr[i].first;
+        }
+        return ans;
+    }
+};
