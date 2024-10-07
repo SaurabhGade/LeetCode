8bbffdfc0ba9d155d3a3d7ea3774f773c0d62bb8
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-10-07 13:33:47 +0530

    Time: 8 ms (42.58%) | Memory: 11.4 MB (72.37%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    string remove(string s){
-         string k = "";
-        for(int i = 0; i  < s.size(); i++){
-            if(s[i] == 'A' && s[i+1] == 'B')
-                i++;
-            else if(s[i] == 'C' && s[i+1] == 'D')
-                i++;
-            else k += s[i];
-        }
-        return k;
-    }
     int minLength(string s) {
-        int ans = s.length();
-        while(true){
-            string k = remove(s);
-            if(k.size() == s.size())
-                return k.size();
-            s = k;
+        stack<char> st;
+        for(int i = 0 ; i < s.size(); i++){
+            if(!st.empty() && st.top() == 'A' && s[i] == 'B')
+                st.pop();
+            else if(!st.empty() && st.top() == 'C' && s[i] == 'D')
+                st.pop();
+            else
+            st.push(s[i]);
         }
+        return st.size();
     }
 };
