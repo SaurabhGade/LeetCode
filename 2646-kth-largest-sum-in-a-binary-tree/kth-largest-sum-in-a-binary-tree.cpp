1074759021cb4eab7a502da7c3b1839306cf9b01
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-10-22 11:35:12 +0530

    Time: 2454 ms (5.08%) | Memory: 182.9 MB (5.08%) - LeetSync

---
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int , long long> mp;
+    void lvl_order_traversal(TreeNode *root, int lv){
+        if(root == NULL)
+            return;
+        lvl_order_traversal(root->left, lv+1);
+        mp[lv] += root->val;
+        lvl_order_traversal(root->right, lv+1);
+    }
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        lvl_order_traversal(root, 0);
+        priority_queue<long long> pq;
+        for(auto i : mp){
+          pq.push(i.second);
+        }
+        if(k > pq.size()) return -1;
+        while(k > 1 && !pq.empty()){
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
