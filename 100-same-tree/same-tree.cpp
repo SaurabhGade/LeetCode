2522531ca08bd5df4254281a32920b498871e2f3
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-11-02 13:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (95.69%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,8 +15,7 @@ public:
         if(p == NULL && q == NULL) return true;
         if(p == NULL || q == NULL) return false;
         if(p->val != q->val) return false;
-        bool x = isSameTree(p->left, q->left);
-        bool y = isSameTree(p->right, q->right);
-        return x&y;
+        return isSameTree(p->left, q->left) && 
+               isSameTree(p->right, q->right);
     }
 };
