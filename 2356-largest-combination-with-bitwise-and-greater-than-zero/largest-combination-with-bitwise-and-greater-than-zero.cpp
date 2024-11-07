227672bf7c9e4200f183b639ca9b7e8c06175ecc
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-11-07 22:31:02 +0530

    Time: 9 ms (96.27%) | Memory: 60.1 MB (74.40%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -6,13 +6,14 @@ public:
         t <<= i;
         return k&t;
     }
+    #define IS_BIT_SET(k, i) (assert((i) <= 31), ((k) & (1U << i)) != 0)
     int largestCombination(vector<int>& c) {
         int n = c.size();
         int ans = 0;
         for(int i = 0; i < 24; i++){
             int cnt = 0;
             for(int j = 0; j < n; j++){
-                cnt += is_bit_set(c[j], i)? 1: 0;
+                cnt += IS_BIT_SET(c[j], i)? 1: 0;
             }
             ans = max(ans, cnt);
         }
