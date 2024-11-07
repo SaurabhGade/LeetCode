cb40c809a95b3373ea4863f915c7774d485a8d27
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-11-07 22:35:31 +0530

    Time: 11 ms (95.52%) | Memory: 60.2 MB (74.40%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -9,7 +9,7 @@ public:
             for(int j = 0; j < n; j++){
                 cnt += IS_BIT_SET(c[j], i)? 1: 0;
             }
-            ans = max(ans, cnt);
+            ans = (cnt > ans)? cnt: ans;
         }
         return ans;
     }
