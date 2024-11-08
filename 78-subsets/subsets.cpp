588f14ba3aaa528364d3e916e70317853ea25ab0
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-11-08 21:34:55 +0530

    Time: 6 ms (5.92%) | Memory: 15.3 MB (8.26%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // itrative solution
     vector<vector<int>> powerSet(vector<int> &nums){
         vector<vector<int>> ans = {{}};
         int n = nums.size();
@@ -14,7 +15,21 @@ public:
         }
         return ans;
     }
+    // recursive solution
+    void powerSetRec(vector<int> nums, vector<int> output, int i, vector<vector<int>> &ans){
+        if(i >= nums.size()){
+            ans.push_back(output);
+            return;
+        }
+        //exclude 
+        powerSetRec(nums, output, i+1, ans);
+        //include
+        output.push_back(nums[i]);
+        powerSetRec(nums, output, i+1, ans);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        return powerSet(nums);
+        vector<vector<int>> ans;
+        powerSetRec(nums, {}, 0, ans);
+        return ans;
     }
 };
