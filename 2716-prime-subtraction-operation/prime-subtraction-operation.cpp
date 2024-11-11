ddab08b2ba55741560f8e4ff340bd2d423cc9144
Saurabh Gade <104384132+SaurabhGade@users.noreply.github.com>
2024-11-11 18:24:42 +0530

    Time: 106 ms (7.25%) | Memory: 26.7 MB (97.12%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    unordered_map<int, bool> mp;
-    bool isSorted(vector<int> & nums){
-        int min = INT_MIN;
-        for(int &i: nums){
-            if(min >= i)
-                return false;
-            min = i;
-        }
+    bool isPrime(int k){
+        int n = sqrt(k);
+        for(int i = 2; i <= n; i++)
+            if(k % i == 0) return false;
         return true;
     }
-    bool isPrime(int k){
-        if(mp.find(k) != mp.end()) return mp[k];
-        if(k == 1) return false;
-        int n = sqrt(k);
-        for(int i = 2; i <= n; i++){
-            if(k % i == 0) {
-                mp[k] = false;
-                return false;
-            }
-        }
-        mp[k] = true;
+    bool isSorted(vector<int> &vec){
+        int min = INT_MIN;
+        for(int &i: vec)
+            if(min >= i) return false;
+            else min = i;
         return true;
     }
     bool primeSubOperation(vector<int>& nums) {
-        int n = nums.size();
         int min = INT_MIN;
+        int n   = nums.size();
         for(int i = 0; i < n; i++){
             int k = nums[i]-1;
+           // if(nums[i] <= min) return false;
             while(k > 1){
                 if(isPrime(k) && nums[i]-k > min){
-                    nums[i] -= k;
-                    break;
+                    nums[i]-=k;
+                     break;
                 }
                 k--;
             }
             min = nums[i];
-            //if(isSorted(nums)) return true;
         }
         return isSorted(nums);
     }
